6b9237a19bec773c13571e67e0977a406b227fb5
Torthu <torstein@gmail.com>
2013-07-01 22:52:58 +0200

    Changed namespace from myApp to microbrewit

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,33 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+angular.module('microbrewit', ['microbrewit.filters', 'microbrewit.services', 'microbrewit.directives', 'microbrewit.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+
+
+    // $routeProvider.when('/user/:userName', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+    $routeProvider.when('/login', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+    $routeProvider.when('/login', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+    $routeProvider.when('/login', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+
+
     $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
+// app.get('/user/:userName', routes.user);
+// app.get('/add/user', routes.addUser);
+// app.get('/login', routes.loginCheck);
+
+// searches
+// app.get('/find/:searchTerms', routes.find);
+// app.get('/find/user/:name', routes.findUser);
+// app.get('/find/brewery/:name', routes.findBrewery);
+// app.get('/find/beer/:name', routes.findBeer);
+
+// beer and breweries
+// app.get('/brewery/:brewery/:id', routes.brewery);
+// app.get('/brewery/:id', routes.brewery);
+// app.get('/brewery/:brewery/:id/:beer', routes.beer);
+
+// add new beers and breweries
+// app.get('/add/brewery', routes.addBrewery);
+// app.get('/add/beer', routes.addBeer);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,7 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
+angular.module('microbrewit.controllers', []).
   controller('MyCtrl1', [function() {
 
   }])
