17ff55f4fc24ad0e0e81b021b63ab78d141fbc07
Torstein Thune <torstein@gmail.com>
2013-07-03 17:31:01 +0200

    Fixed directive for gravatars, added directive for assigning active class to navigational elements

--- app/js/directives.js
+++ app/js/directives.js
@@ -5,14 +5,44 @@
 
 angular.module('microbrewit.directives', []).
   directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
+    return function (scope, elm, attrs) {
       elm.text(version);
     };
   }]).
   directive('gravatar', function() {
-	var returnz = md5(contents(elem).replace(/\s+/g, '').toLowerCase());
 	return {
-		restrict: "E",
-		template: "<img src='http://gravata.com/avatar/"+returnz+"'' />"
-	};
-});
+		restrict: 'E',
+		scope: 'isolate',
+		replace: true,
+		transclude: true,
+		template: '<div class="avatar gravatar"><img src="http://www.gravatar.com/avatar/{{email | gravatar}}" alt="" /></div>',
+		compile: function (element, attr, transclusionFunc) {
+			return function (scope, iterStartElement, attr) {
+				var origElem = transclusionFunc(scope);
+				var content =  origElem.text();
+				console.log(content);
+				scope.mail = content;
+            };
+        }
+    };
+}).
+  directive('activeLink', ['$location', function(location) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs, controller) {
+            var clazz = attrs.activeLink;
+            var path = attrs.href;
+            path = path.substring(1); //hack because path does bot return including hashbang
+            scope.location = location;
+            scope.$watch('location.path()', function(newPath) {
+                if (path === newPath) {
+                    element.addClass(clazz);
+                } else {
+                    element.removeClass(clazz);
+                }
+            });
+        }
+
+    };
+
+}]);
