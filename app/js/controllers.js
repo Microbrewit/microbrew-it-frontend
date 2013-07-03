9d7491aa797d67242f8f75945ee71c5bcfa230e9
Torstein Thune <torstein@gmail.com>
2013-07-03 20:23:40 +0200

    added controller for add beer

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,13 +6,12 @@
 angular.module('microbrewit.controllers', []).
   controller('BeerCtrl', [function($scope) {
   	$scope.title = "Brun Bjarne";
-
   }]).
   controller('RegisterCtrl', function($scope) {
-	  $scope.email = "Torstein@gmail.com";
-
   }).
   controller('BreweryCtrl', [function($scope) {
-  	// $scope.email = "Test@gmail.com";
-
-  }]);
+  }]).
+  controller('RecipeCtrl', function($scope) {
+	$scope.beerName = "Brun Bjarne v2";
+	$scope.forkOf = "Brun Bjarne";
+  });
