2a04955b86208e8e1ef39e944ea6b06d1b907ab2
Torthu <torstein@gmail.com>
2013-07-08 13:09:52 +0200

    Rewrote gravatar directive, added directive for errormessages

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,45 +4,49 @@
 
 
 angular.module('microbrewit.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function (scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]).
-  directive('gravatar', function() {
-	return {
-		restrict: 'E',
-		scope: 'isolate',
-		replace: true,
-		transclude: true,
-		template: '<div class="avatar gravatar"><img src="http://www.gravatar.com/avatar/{{email | gravatar}}" alt="" /></div>',
-		compile: function (element, attr, transclusionFunc) {
-			return function (scope, iterStartElement, attr) {
-				var origElem = transclusionFunc(scope);
-				var content =  origElem.text();
-				console.log(content);
-				scope.mail = content;
-            };
-        }
-    };
-}).
-  directive('activeLink', ['$location', function(location) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs, controller) {
-            var clazz = attrs.activeLink;
-            var path = attrs.href;
-            path = path.substring(1); //hack because path does bot return including hashbang
-            scope.location = location;
-            scope.$watch('location.path()', function(newPath) {
-                if (path === newPath) {
-                    element.addClass(clazz);
-                } else {
-                    element.removeClass(clazz);
-                }
-            });
-        }
+    directive('gravatar', function() {
+        return {
+            restrict: 'E',
+            scope: 'isolate',
+            replace: true,
+            template: '<div class="avatar gravatar"><img src="http://www.gravatar.com/avatar/{{src}}" style="width:{{size}};height:{{size}}" alt="" /></div>',
+            link: function (scope, element, attr) {
+                attr.$observe('src', function(current_value) {
+                    scope.src = md5(current_value.replace(/\s+/g, '').toLowerCase()); // create gravatar hash of interpolated value, push to scope
+                });
 
-    };
+                scope.size = attr.size; // set size (width = height) of gravatar
+            }
+        };
+    }).
+    directive('activeLink', ['$location', function(location) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs, controller) {
+                var clazz = attrs.activeLink;
+                var path = attrs.href;
+                path = path.substring(1); //hack because path does bot return including hashbang
+                scope.location = location;
+                scope.$watch('location.path()', function(newPath) {
+                    if (path === newPath) {
+                        element.addClass(clazz);
+                    } else {
+                        element.removeClass(clazz);
+                    }
+                });
+            }
+        };
+    }]).
+    directive('mbError', function() {
+        return {
+            restrict: 'A',
+            template: '<div class="error-message>{{errormessage}}</div>',
+            link: function (scope, element, attr) {
+                // attr.$observe('src', function(current_value) {
+                //     scope.src = md5(current_value.replace(/\s+/g, '').toLowerCase()); // create gravatar hash of interpolated value, push to scope
+                // });
 
-}]);
+                scope.errormessage = element.text(); // set size (width = height) of gravatar
+            }
+        };
+    });
