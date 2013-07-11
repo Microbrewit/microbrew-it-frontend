becb040e7e3c0396cff4cb3a7a2dfac3bf87a028
Torthu <torstein@gmail.com>
2013-07-11 15:14:02 +0200

    added route to privacy policy

--- app/js/app.js
+++ app/js/app.js
@@ -13,6 +13,8 @@ angular.module('microbrewit', ['microbrewit.filters', 'microbrewit.services', 'm
       .when('/profile', {templateUrl: 'partials/profile.html', controller: 'ProfileCtrl'})
       .when('/calculators', {templateUrl: 'partials/calculators.html', controller: 'CalculatorCtrl'})
       .when('/calculators/abv', {templateUrl: 'partials/calculators/abv.html', controller: 'CalculatorCtrl'})
-      .when('/', {templateUrl: 'partials/index.html', controller: 'BreweryCtrl'});
+      .when('/company/privacy-policy', {templateUrl: 'partials/company/privacy-policy.html', controller: 'CalculatorCtrl'})
+      .when('/more', {templateUrl: 'partials/more.html'})
+      .when('/', {templateUrl: 'partials/index.html'});
       $routeProvider.otherwise({templateUrl: 'partials/404.html'});
   	}]);
